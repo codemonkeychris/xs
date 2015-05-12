89eed994ca5833ea32c5b45d4fea5b3304822e45
Chris Anderson <chrisan@outlook.com>
2015-05-12 14:50:19 -0700

    missing files

---
+++ src/XSRT2/RuntimeHelpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using System.Reflection;
+using System.IO;
+
+namespace XSRT2
+{
+    public static class RuntimeHelpers
+    {
+        static async Task<string> GetResource(string resource)
+        {
+            string text;
+            using (var stream = typeof(RuntimeHelpers).GetTypeInfo().Assembly.GetManifestResourceStream(resource))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    text = await reader.ReadToEndAsync();
+                }
+            }
+            return text;
+        }
+
+        public static IAsyncOperation<string> GetRuntimeJavaScript()
+        {
+            var t = GetResource("XSRT2.xsrt.js");
+            return t.AsAsyncOperation<string>();
+        }
+    }
+}
---
+++ src/XSRT2/xsrt.js
@@ -0,0 +1,15 @@
+﻿function render(ev) {
+    try {
+        ev.view = JSON.stringify((App && App.render) ? App.render() : { type: 'TextBlock', text: 'Error: App.render not found' });
+    }
+    catch (e) {
+        ev.view = JSON.stringify({ type: 'TextBlock', text: 'Error: ' + e });
+    }
+}
+function command(ev) {
+    var handler = App && App.eventHandlers && App.eventHandlers[ev.commandHandlerToken];
+    if (handler) { handler(ev.sender, ev.eventArgs); }
+}
+host.state.addEventListener('render', render);
+host.state.addEventListener('command', command);
+if (App && App.setInitialState) { App.setInitialState(); }
