a91813b26d5ac5a0f9dfa432c61b6691912b9ba0
Chris Anderson <chrisan@outlook.com>
2015-05-16 17:05:20 -0700

    move all the hosting code into utility library

--- src/XSTest/MainPage.xaml.cs
+++ src/XSTest/MainPage.xaml.cs
@@ -26,168 +26,34 @@ namespace XSTest
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        XSRT.JScriptRuntime jsrt = null;
-        XSRT2.StateManager state = new XSRT2.StateManager();
-        Diff diff;
+        Host host;
         DispatcherTimer dt;
         int frame;
-        string lastProgram = "";
 
         public MainPage()
         {
             this.InitializeComponent();
             this.Content = new ContentControl();
-            diff = new Diff(state, (ContentControl)this.Content);
-            Diff.Command += delegate (object sender, CommandEventArgs e)
-            {
-                state.NotifyCommand(e);
-                RenderIfNeeded();
-            };
-            Startup();
-            
-        }
-        async void Startup()
-        {
-            state.SetState("frame", (frame++).ToString());
-            await InitFile();
-            await CheckFile();
+            host = new Host((ContentControl)this.Content) { AutoCheckUpdates = true };
+            host.State.SetState("frame", (frame++).ToString());
+            host.Startup();
+
             dt = new DispatcherTimer();
-#if STRESS_RELOAD
-            dt.Interval = TimeSpan.FromMilliseconds(16);
-#else
             dt.Interval = TimeSpan.FromMilliseconds(1000);
-#endif
             dt.Tick += dt_Tick;
             dt.Start();
         }
 
-        void RenderIfNeeded()
-        {
-            try
-            {
-                Display(state.RenderIfNeeded());
-            }
-            catch (Exception x)
-            {
-                Display(new RenderEventArgs() { View = "{ type:'TextBlock', text:'Error:" + x.ToString().Replace("\'", "\"") + "' }" });
-                return;
-            }
-        }
-
         private void dt_Tick(object sender, object e)
         {
-            state.SetState("frame", (frame++).ToString());
-            CheckFile();
-            RenderIfNeeded();
-        }
-        const string path = "xs-program.js";
-        const string defaultApp = @"
-var App;
-(function (App) {
-    App.render = function() {
-        return { type:'TextBlock', text:'Hello from JS!' };
-    }
-    
-})(App || (App = {}));
-";
-        async Task<string> InitFile()
-        {
-
-            StorageFile file;
-            try
-            {
-                var found = await Windows.Storage.ApplicationData.Current.RoamingFolder.TryGetItemAsync(path);
-                if (found == null)
-                {
-                    file = await Windows.Storage.ApplicationData.Current.RoamingFolder.CreateFileAsync(path, Windows.Storage.CreationCollisionOption.FailIfExists);
-                    await FileIO.WriteTextAsync(file, defaultApp);
-                }
-                else
-                {
-                    file = (StorageFile)found;
-                }
-            }
-            catch
-            {
-                file = await Windows.Storage.ApplicationData.Current.RoamingFolder.CreateFileAsync(path, CreationCollisionOption.OpenIfExists);
-            }
-            return await ReadText(file);
-        }
-
-        // This is here purely as a debugging aid for hard coding content of the file in cases
-        // where there is a problem
-        // 
-        async Task<string> ReadText(StorageFile file)
-        {
-            try {
-                var str = await FileIO.ReadTextAsync(file);
-                return str;
-            }
-            catch (UnauthorizedAccessException)
-            {
-                // need wait
-                return @"var App;
-                (function(App) {
-                    App.render = function() {
-                        return { type:'TextBlock', text:'Failed to load file (access denied)' };
-                    }
-
-                })(App || (App = { }));";
-            }
-        }
-
-        async Task<string> CheckFile()
-        {
-            var file = await Windows.Storage.ApplicationData.Current.RoamingFolder.CreateFileAsync(path, Windows.Storage.CreationCollisionOption.OpenIfExists);
-            //var file = await Windows.Storage.KnownFolders.DocumentsLibrary.CreateFileAsync(path, Windows.Storage.CreationCollisionOption.OpenIfExists);
-            var contents = await ReadText(file);
-            bool changed = lastProgram != contents;
-#if STRESS_RELOAD
-            var b = new byte[1];
-            new Random().NextBytes(b);
-            changed = changed || (b[0] < 50); // 20% chance of random reload
-#endif
-            if (changed)
-            {
-                lastProgram = contents;
-                var runtime = await RuntimeHelpers.GetRuntimeJavaScript();
-                Initialize(contents, runtime);
-            }
-            return contents;
-        }
-
-        void Initialize(string program, string runtime)
-        {
-            if (jsrt != null)
-            {
-                state.ReleaseEventHandlers();
-                jsrt.ClearActive();
-                jsrt = null;
-            }
-            state.NotifyChanged();
-
-            jsrt = new XSRT.JScriptRuntime();
-            jsrt.SetActive();
-            // must call AddWinRTNamespace before AddHostObject
-            //
-            jsrt.AddWinRTNamespace("Windows"); 
-            jsrt.AddWinRTNamespace("XSRT2"); 
-            jsrt.AddHostObject("state", state);
-            jsrt.Eval(program + "\r\n" + runtime);
-        }
-
-        private void Display(RenderEventArgs renderEventArgs)
-        {
-            if (renderEventArgs != null)
-            {
-                diff.Process(renderEventArgs.View.ToString());
-            }
+            host.State.SetState("frame", (frame++).ToString());
+            host.RenderIfNeeded();
         }
 
         protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
         {
-            jsrt.ClearActive();
-            jsrt = null;
+            host.Close();
+            host = null;
             base.OnNavigatingFrom(e);
         }
     }
