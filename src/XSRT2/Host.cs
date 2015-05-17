966003f90158559dbb57eb53d74aa9b1ed6fd819
Chris Anderson <chrisan@outlook.com>
2015-05-16 17:05:34 -0700

    move all the hosting code into utility library

---
+++ src/XSRT2/Host.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace XSRT2
+{
+    public sealed class Host
+    {
+        XSRT.JScriptRuntime jsrt = null;
+        XSRT2.StateManager state = new XSRT2.StateManager();
+        Diff diff;
+        string lastProgram = "";
+        bool autoCheckUpdates = false;
+        DispatcherTimer dt;
+        const string path = "xs-program.js";
+        const string defaultApp = @"
+var App;
+(function (App) {
+    App.render = function() {
+        return { type:'TextBlock', text:'Hello from JS!' };
+    }
+    
+})(App || (App = {}));
+";
+
+        public Host(ContentControl displayControl)
+        {
+            diff = new Diff(state, displayControl);
+            Diff.Command += delegate (object sender, CommandEventArgs e)
+            {
+                state.NotifyCommand(e);
+                RenderIfNeeded();
+            };
+        }
+
+        public bool StressReload { get; set; }
+
+        public StateManager State { get { return state; } }
+
+        public bool AutoCheckUpdates
+        {
+            get { return autoCheckUpdates; }
+            set { autoCheckUpdates = value; UpdateAutoTimer(); }
+        }
+
+        void UpdateAutoTimer()
+        {
+            if (dt != null)
+            {
+                dt.Stop();
+                dt = null;
+            }
+
+            if (autoCheckUpdates)
+            {
+                dt = new DispatcherTimer();
+                dt.Interval = TimeSpan.FromMilliseconds(250);
+                dt.Tick += autoTimer_Tick;
+                dt.Start();
+            }
+        }
+
+        void autoTimer_Tick(object sender, object e)
+        {
+            CheckForUpdates();
+        }
+
+        public async void Startup()
+        {
+            await InitFile();
+            await CheckFile();
+            RenderIfNeeded();
+        }
+        public async void CheckForUpdates()
+        {
+            await CheckFile();
+            RenderIfNeeded();
+        }
+        public void Close()
+        {
+            jsrt.ClearActive();
+            jsrt = null;
+        }
+        public void RenderIfNeeded()
+        {
+            try
+            {
+                Display(state.RenderIfNeeded());
+            }
+            catch (Exception x)
+            {
+                Display(new RenderEventArgs() { View = "{ type:'TextBlock', text:'Error:" + x.ToString().Replace("\'", "\"") + "' }" });
+                return;
+            }
+        }
+
+        async Task<string> InitFile()
+        {
+
+            StorageFile file;
+            try
+            {
+                var found = await Windows.Storage.ApplicationData.Current.RoamingFolder.TryGetItemAsync(path);
+                if (found == null)
+                {
+                    file = await Windows.Storage.ApplicationData.Current.RoamingFolder.CreateFileAsync(path, Windows.Storage.CreationCollisionOption.FailIfExists);
+                    await FileIO.WriteTextAsync(file, defaultApp);
+                }
+                else
+                {
+                    file = (StorageFile)found;
+                }
+            }
+            catch
+            {
+                file = await Windows.Storage.ApplicationData.Current.RoamingFolder.CreateFileAsync(path, CreationCollisionOption.OpenIfExists);
+            }
+            return await ReadText(file);
+        }
+
+        // This is here purely as a debugging aid for hard coding content of the file in cases
+        // where there is a problem
+        // 
+        async Task<string> ReadText(StorageFile file)
+        {
+            try
+            {
+                var str = await FileIO.ReadTextAsync(file);
+                return str;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // need wait
+                return @"var App;
+                (function(App) {
+                    App.render = function() {
+                        return { type:'TextBlock', text:'Failed to load file (access denied)' };
+                    }
+
+                })(App || (App = { }));";
+            }
+        }
+
+        async Task<string> CheckFile()
+        {
+            var file = await Windows.Storage.ApplicationData.Current.RoamingFolder.CreateFileAsync(path, Windows.Storage.CreationCollisionOption.OpenIfExists);
+            //var file = await Windows.Storage.KnownFolders.DocumentsLibrary.CreateFileAsync(path, Windows.Storage.CreationCollisionOption.OpenIfExists);
+            var contents = await ReadText(file);
+            bool changed = lastProgram != contents;
+
+            if (StressReload)
+            {
+                var b = new byte[1];
+                new Random().NextBytes(b);
+                changed = changed || (b[0] < 50); // 20% chance of random reload
+            }
+            if (changed)
+            {
+                lastProgram = contents;
+                var runtime = await RuntimeHelpers.GetRuntimeJavaScript();
+                Initialize(contents, runtime);
+            }
+            return contents;
+        }
+
+        void Initialize(string program, string runtime)
+        {
+            if (jsrt != null)
+            {
+                state.ReleaseEventHandlers();
+                jsrt.ClearActive();
+                jsrt = null;
+            }
+            state.NotifyChanged();
+
+            jsrt = new XSRT.JScriptRuntime();
+            jsrt.SetActive();
+            // must call AddWinRTNamespace before AddHostObject
+            //
+            jsrt.AddWinRTNamespace("Windows");
+            jsrt.AddWinRTNamespace("XSRT2");
+            jsrt.AddHostObject("state", state);
+            jsrt.Eval(program + "\r\n" + runtime);
+        }
+
+        void Display(RenderEventArgs renderEventArgs)
+        {
+            if (renderEventArgs != null)
+            {
+                diff.Process(renderEventArgs.View.ToString());
+            }
+        }
+    }
+}
